55c24ec72f02018e83f2e79ce4ef361f77659927
Zairaxx <brandontsegai@gmail.com>
2025-05-22 11:42:57 +0200

    added questions

--- src/components/questionSets.js
+++ src/components/questionSets.js
@@ -99,5 +99,38 @@ export const questionsSetTwo = [
     "type": "trueFalse",
     "statement": "Koldioxid som släpps ut vid förbränning kan återanvändas i fotosyntesen.",
     "correct": true
-  }
+  },
+  {
+  "type": "multipleChoice",
+  "question": "Vilken är den främsta anledningen till att en kemisk reaktion kräver startenergi?",
+  "options": [
+    "För att skapa nya atomer innan reaktionen börjar",
+    "För att kyla ner ämnena så att reaktionen kan kontrolleras",
+    "För att öka mängden energi i produkterna",
+    "För att bryta de kemiska bindningarna i de reagerande ämnena"
+  ],
+  "correctAnswer": "För att bryta de kemiska bindningarna i de reagerande ämnena"
+},
+{
+  "type": "multipleChoice",
+  "question": "Vilket separationssätt är bäst lämpat för att separera salt från en saltlösning?",
+  "options": [
+    "Destillering",
+    "Filtrering",
+    "Indunstning",
+    "Sedimentering"
+  ],
+  "correctAnswer": "Indunstning"
+},
+{
+  "type": "multipleChoice",
+  "question": "Vad beskriver bäst skillnaden mellan temperatur och värmeenergi?",
+  "options": [
+    "Temperatur är mängden energi ett föremål innehåller, medan värmeenergi är hur snabbt partiklarna rör sig.",
+    "Värmeenergi är ett mått på den genomsnittliga rörelseenergin hos partiklar, medan temperatur är den energi som överförs mellan objekt.",
+    "Temperatur är ett mått på hur mycket ett ämne väger, medan värmeenergi är ett mått på dess densitet.",
+    "Temperatur är ett mått på partiklarnas genomsnittliga rörelseenergi, medan värmeenergi är den totala energi som överförs på grund av temperaturskillnader."
+  ],
+  "correctAnswer": "Temperatur är ett mått på partiklarnas genomsnittliga rörelseenergi, medan värmeenergi är den totala energi som överförs på grund av temperaturskillnader."
+}
 ]
